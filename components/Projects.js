0888bfcd4b44f825d14317a31916a5c8485291c8
arito7 <64824933+arito7@users.noreply.github.com>
2022-04-01 15:06:22 +0900

    Adjust project card spacing

--- components/Projects.js
+++ components/Projects.js
@@ -1,34 +1,39 @@
 import styles from '../styles/Projects.module.css';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Projects = () => {
+  const router = useRouter();
   const projects = [
     {
       title: 'CRUD Inventory App',
       img: 'fake-inventory.png',
       desc: 'A CRUD app using express and ejs',
+      link: 'https://github.com/arito7/inventory-application',
     },
   ];
   return (
     <section className={styles.projects}>
       <h2 className="section-title">Projects</h2>
       <ul className={styles.list}>
-        {projects.map(({ title, img, desc }) => {
+        {projects.map(({ title, img, desc, link }) => {
           return (
-            <li key={title} style={{ maxWidth: '40%' }}>
-              <div className={styles.card}>
-                <h3>{title}</h3>
+            <li key={title} className={styles.card}>
+              <div
+                onClick={() => {
+                  router.push(link);
+                }}
+              >
+                <h3 className={styles.projecttitle}>{title}</h3>
                 <Image
-                  style={{
-                    borderRadius: '1rem',
-                  }}
+                  className={styles.projectimg}
                   layout="responsive"
                   height={795}
                   width={787}
                   alt="Project Image"
                   src={`/images/${img}`}
                 />
-                <p>{desc}</p>
+                <p className={styles.projectdesc}>{desc}</p>
               </div>
             </li>
           );
